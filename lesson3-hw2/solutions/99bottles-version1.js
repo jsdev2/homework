3f0470c724cb4368ca9305e3c109aade0db47a85
Richard Harrington <harrington.richard@gmail.com>
2016-05-13 12:11:27 -0400

    broke 99bottles versions into 2 files

--- lesson3-hw2/solutions/99bottles-version1.js
+++ lesson3-hw2/solutions/99bottles-version1.js
@@ -26,27 +26,3 @@ for (beers = 99; beers >= 0; beers--) {
                     "99 bottles of beer on the wall.");
     }
 }
-
-// Solution 2:
-
-function bottlePhrase(n) {
-    if (n > 1) {
-        return n + " bottles";
-    } else if (n === 1) {
-        return "1 bottle";
-    } else {
-        return "no more bottles";
-    }
-}
-
-for (beers = 99; beers >= 1; beers--) {
-    console.log(bottlePhrase(beers) + " of beer on the wall, " + 
-                bottlePhrase(beers) + " of beer.\n" +
-                "Take one down and pass it around, " + 
-                bottlePhrase(beers - 1) + " of beer on the wall.");
-}
-
-console.log("No more bottles of beer on the wall, " + 
-            "no more bottles of beer.\n" +
-            "Go to the store and buy some more, " + 
-            "99 bottles of beer on the wall.");
